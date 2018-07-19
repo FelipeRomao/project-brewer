ba530b3c9d0a630baab07f60a9d30f0732d86116
Felipe Romao <feliperomao.a@gmail.com>
2018-07-18 21:27:50 -0300

    Implementando o Forward e Redirect

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -3,9 +3,11 @@ package com.algaworks.brewer.controller;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.brewer.model.Cerveja;
 
@@ -18,12 +20,13 @@ public class CervejasController {
 	}
 	
 	@PostMapping(value = "/cervejas/novo")
-	public String cadastrar(@Valid Cerveja cerveja, BindingResult result) {
+	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
 		if(result.hasErrors()) {
-			System.out.println("Ocorreu um erro sim!");
+			model.addAttribute("mensagem", "Erro ao tentar salvar o cadastro");
+			return "cerveja/CadastroCerveja";
 		}
 		
-		System.out.println(">>>>> sku : " + cerveja.getSku());
-		return "cerveja/CadastroCerveja";
+		attributes.addFlashAttribute("mensagem", "Cadastro salvo com sucesso!");
+		return "redirect:/cervejas/novo";
 	}
 }
