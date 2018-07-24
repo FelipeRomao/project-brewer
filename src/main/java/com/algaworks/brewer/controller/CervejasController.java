1d4e875be9df401a8abf838ac0369fadb0d56e7e
Felipe Romao <feliperomao.a@gmail.com>
2018-07-23 21:53:32 -0300

    Baixando e configurando o layout no projeto

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -26,5 +27,10 @@ public class CervejasController {
 		
 		attributes.addFlashAttribute("mensagem", "Cadastro salvo com sucesso!");
 		return "redirect:/cervejas/novo";
+	}
+	
+	@GetMapping("/cervejas/cadastroCerveja")
+	public String cadastroCerveja() {
+		return "cerveja/cadastro-produto";
 	}
 }
