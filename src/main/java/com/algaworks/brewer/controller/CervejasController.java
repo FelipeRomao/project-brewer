b6b8a0bc17e049de4259621c079d827b1c7b8208
Felipe Romao <feliperomao.a@gmail.com>
2018-07-24 22:56:10 -0300

    Prototipando o cadastro da cerveja

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -11,26 +11,22 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.brewer.model.Cerveja;
 
-@Controller
+@Controller("/cervejas")
 public class CervejasController {
 
-	@RequestMapping("/cervejas/novo")
+	@RequestMapping("/novo")
 	public String novo(Cerveja cerveja) {
 		return "cerveja/CadastroCerveja";
 	}
-	
-	@PostMapping(value = "/cervejas/novo")
+
+	@PostMapping(value = "/novo")
 	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, RedirectAttributes attributes) {
-		if(result.hasErrors()) {
+		if (result.hasErrors()) {
 			return novo(cerveja);
 		}
-		
+
 		attributes.addFlashAttribute("mensagem", "Cadastro salvo com sucesso!");
 		return "redirect:/cervejas/novo";
 	}
-	
-	@GetMapping("/cervejas/cadastroCerveja")
-	public String cadastroCerveja() {
-		return "cerveja/cadastro-produto";
-	}
+
 }
