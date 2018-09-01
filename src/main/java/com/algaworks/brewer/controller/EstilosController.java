a0547ac34bcaba8e77acf664a6e67cd97ca4862d
Felipe Romao <feliperomao.a@gmail.com>
2018-08-31 22:40:36 -0300

    Implementando o cadastro do estilo

--- src/main/java/com/algaworks/brewer/controller/EstilosController.java
+++ src/main/java/com/algaworks/brewer/controller/EstilosController.java
@@ -1,15 +1,40 @@
 package com.algaworks.brewer.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.algaworks.brewer.model.Estilo;
+import com.algaworks.brewer.service.CadastroEstiloService;
 
 @Controller
 @RequestMapping("/estilos")
 public class EstilosController {
 
+	@Autowired
+	private CadastroEstiloService cadastroEstiloService;
+
 	@GetMapping("/novo")
-	public String novo() {
-		return "/estilo/CadastroEstilo";
+	public ModelAndView novo(Estilo estilo) {
+		return new ModelAndView("estilo/CadastroEstilo");
+	}
+
+	@PostMapping("/novo")
+	public ModelAndView cadastrar(@Valid Estilo estilo, BindingResult result, RedirectAttributes attributes) {
+		if (result.hasErrors()) {
+			return novo(estilo);
+		}
+
+		cadastroEstiloService.salvar(estilo);
+		attributes.addFlashAttribute("mensagem", "Estilo salvo com sucesso");
+
+		return new ModelAndView("redirect:/estilos/novo");
 	}
 }
