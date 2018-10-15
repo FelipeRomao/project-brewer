23f0b8ad8162d694cf9700fb415890d2f8643dc8
Felipe Romao <feliperomao.a@gmail.com>
2018-10-14 21:55:04 -0300

    Paginacao na pesquisa de cervejas

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -4,6 +4,8 @@ package com.algaworks.brewer.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -55,13 +57,18 @@ public class CervejasController {
 	}
 	
 	@GetMapping
-	public ModelAndView pesquisar(CervejaFilter cervejaFilter, BindingResult result) {
+	public ModelAndView pesquisar(CervejaFilter cervejaFilter, BindingResult result, @PageableDefault(size = 2) Pageable pageable) {
 		ModelAndView mv = new ModelAndView("cerveja/PesquisaCervejas");
 		mv.addObject("estilos", estilos.findAll());
 		mv.addObject("sabores", Sabor.values());
 		mv.addObject("origens", Origem.values());
 		
-		mv.addObject("cervejas", cervejas.filtrar(cervejaFilter));
+		System.out.println(">>>>> number : " + pageable.getPageNumber());
+		System.out.println(">>>>> page size : " + pageable.getPageSize());
+		
+		mv.addObject("cervejas", cervejas.filtrar(cervejaFilter, pageable));
+		
+		
 		return mv;
 	}
 
