516cb9da6f585e63a2adec45614550c192ad50ab
Felipe Romao <feliperomao.a@gmail.com>
2018-12-04 22:28:55 -0200

    Input e componente para Data

--- target/classes/static/javascripts/brewer.js
+++ target/classes/static/javascripts/brewer.js
@@ -62,6 +62,26 @@ Brewer.MaskCep = (function(){
 	
 }());
 
+Brewer.MaskDate = (function() {
+	
+	function MaskDate() {
+		this.inputDate = $('.js-date');
+	}
+	
+	MaskDate.prototype.enable = function() {
+		this.inputDate.mask('00/00/0000');
+		
+		this.inputDate.datepicker({
+			orientation : 'bottom',
+			language : 'pt-BR',
+			autoclose : 'true'
+		});
+	}
+	
+	return MaskDate;
+	
+}());
+
 $(function() {
 	var maskMoney = new Brewer.MaskMoney();
 	maskMoney.enable();
@@ -71,4 +91,7 @@ $(function() {
 	
 	var maskCep = new Brewer.MaskCep();
 	maskCep.enable();
+	
+	var maskDate = new Brewer.MaskDate();
+	maskDate.enable();
 });
