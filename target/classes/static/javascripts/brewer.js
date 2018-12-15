41286e763a02375eb4f1991ef0998f2f340a9ccc
Felipe Romao <feliperomao.a@gmail.com>
2018-12-15 12:27:34 -0200

    Configurando o CSRF

--- target/classes/static/javascripts/brewer.js
+++ target/classes/static/javascripts/brewer.js
@@ -82,6 +82,23 @@ Brewer.MaskDate = (function() {
 	
 }());
 
+Brewer.Security = (function() {
+	
+	function Security() {
+		this.token = $('input[name=_csrf]').val();
+		this.header = $('input[name=_csrf_header]').val();
+	}
+	
+	Security.prototype.enable = function() {
+		$(document).ajaxSend(function(event, jqxhr, settings){
+			jqxhr.setRequestHeader(this.header, this.token);
+		}.bind(this));
+	}
+	
+	return Security;
+	
+}());
+ 
 $(function() {
 	var maskMoney = new Brewer.MaskMoney();
 	maskMoney.enable();
@@ -94,4 +111,7 @@ $(function() {
 	
 	var maskDate = new Brewer.MaskDate();
 	maskDate.enable();
+	
+	var security = new Brewer.Security();
+	security.enable();
 });
