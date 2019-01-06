85ef24e629d3bc9892eebd977f4fd47ed28d6186
Felipe Romao <feliperomao.a@gmail.com>
2019-01-06 17:33:17 -0200

    Renderizando cervejas no autocomplete

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,10 +35,10 @@ public class CervejasController {
 
 	@Autowired
 	private Estilos estilos;
-	
+
 	@Autowired
 	private CadastroCervejaService cadastroCervejaService;
-	
+
 	@Autowired
 	private Cervejas cervejas;
 
@@ -52,32 +53,32 @@ public class CervejasController {
 
 	@PostMapping(value = "/novo")
 	public ModelAndView cadastrar(@Valid Cerveja cerveja, BindingResult result, RedirectAttributes attributes) {
-		if(result.hasErrors()) {
+		if (result.hasErrors()) {
 			return novo(cerveja);
 		}
-		
+
 		cadastroCervejaService.salvar(cerveja);
 		attributes.addFlashAttribute("mensagem", "Cerveja salva com sucesso!");
-		
+
 		return new ModelAndView("redirect:/cervejas/novo");
 	}
-	
+
 	@GetMapping
-	public ModelAndView pesquisar(CervejaFilter cervejaFilter, BindingResult result, 
+	public ModelAndView pesquisar(CervejaFilter cervejaFilter, BindingResult result,
 			@PageableDefault(size = 2) Pageable pageable, HttpServletRequest httpServletRequest) {
 		ModelAndView mv = new ModelAndView("cerveja/PesquisaCervejas");
 		mv.addObject("estilos", estilos.findAll());
 		mv.addObject("sabores", Sabor.values());
 		mv.addObject("origens", Origem.values());
-		
+
 		PageWrapper<Cerveja> paginaWrapper = new PageWrapper<>(cervejas.filtrar(cervejaFilter, pageable),
-				 httpServletRequest);
+				httpServletRequest);
 		mv.addObject("pagina", paginaWrapper);
-		
+
 		return mv;
 	}
-	
-	@GetMapping("/filtro")
+
+	@RequestMapping(consumes = { MediaType.APPLICATION_JSON_VALUE })
 	public @ResponseBody List<CervejaDTO> pesquisar(String skuOuNome) {
 		return cervejas.porSkuOuNome(skuOuNome);
 	}
