db9332cc8906158b8de2c819eeaba89a182e27f0
Felipe Romao <feliperomao.a@gmail.com>
2019-01-05 22:50:24 -0200

    Selecionando o cliente na pesquisa rapida

--- src/main/resources/static/javascripts/cliente.pesquisa-rapida.js
+++ src/main/resources/static/javascripts/cliente.pesquisa-rapida.js
@@ -10,11 +10,17 @@ Brewer.PesquisaRapidaCliente = (function() {
 		this.htmlTabelaPesquisa = $('#tabela-pesquisa-rapida-cliente').html();
 		this.template = Handlebars.compile(this.htmlTabelaPesquisa);
 		this.mensagemErro = $('.js-mensagem-erro');
+		this.cliente = $('.js-cliente-pesquisa-rapida');
 	}
 	
 	PesquisaRapidaCliente.prototype.iniciar = function() {
 		this.pesquisaRapidaBtn.on('click', onPesquisaRapidaClicado.bind(this));
+		this.pesquisaRapidaClientesModal.on('shown.bs.modal', onModalShow.bind(this));
 		
+	}
+	
+	function onModalShow() {
+		this.nomeInput.focus();
 	}
 	
 	function onPesquisaRapidaClicado(event) {
@@ -33,9 +39,13 @@ Brewer.PesquisaRapidaCliente = (function() {
 	}
 	
 	function onPesquisaConcluida(resultado) {
+		this.mensagemErro.addClass('hidden');
+		
 		var html = this.template(resultado);
 		this.containerTabelaPesquisa.html(html);
-		this.mensagemErro.addClass('hidden');
+		
+		var tabelaClientePesquisaRapida = new Brewer.TabelaClientePesquisaRapida(this.pesquisaRapidaClientesModal);
+		tabelaClientePesquisaRapida.iniciar();
 	}
 	
 	function onErroPesquisa() {
@@ -43,6 +53,30 @@ Brewer.PesquisaRapidaCliente = (function() {
 	}
 	
 	return PesquisaRapidaCliente;
+	
+}());
+
+Brewer.TabelaClientePesquisaRapida = (function() {
+		
+	function TabelaClientePesquisaRapida(modal) {
+		this.modalCliente = modal;
+		this.cliente = $('.js-cliente-pesquisa-rapida');
+	}
+	
+	TabelaClientePesquisaRapida.prototype.iniciar = function() {
+		this.cliente.on('click', onClienteSelecionado.bind(this));
+	}
+	
+	function onClienteSelecionado(evento) {
+		this.modalCliente.modal('hide');
+		
+		var clienteSelecionado = $(evento.currentTarget);
+		$('#nomeCliente').val(clienteSelecionado.data('nome'));
+		$('#codigoCliente').val(clienteSelecionado.data('codigo'));
+		
+	}
+	
+	return TabelaClientePesquisaRapida;
 	
 }());
 
