913fcffcad40c3066638d8e3476e6788fae6c735
Felipe Romao <feliperomao.a@gmail.com>
2019-01-09 14:59:20 -0200

    Adicionando mesmos itens na venda

--- src/main/resources/static/javascripts/venda.autocomplete-itens.js
+++ src/main/resources/static/javascripts/venda.autocomplete-itens.js
@@ -13,8 +13,8 @@ Brewer.AutoComplete = (function() {
 	AutoComplete.prototype.iniciar = function() {
 		var options = {
 			url : function(skuOuNome) {
-				return '/brewer/cervejas?skuOuNome=' + skuOuNome;
-			},
+				return this.skuOuNomeInput.data('url') + '?skuOuNome=' + skuOuNome;
+			}.bind(this),
 			getValue : 'nome',
 			minCharNumber: 3,
 			requestDelay : 300,
@@ -35,6 +35,8 @@ Brewer.AutoComplete = (function() {
 	
 	function onItemSelecionado() {
 		this.emitter.trigger('item-selecionado', this.skuOuNomeInput.getSelectedItemData());
+		this.skuOuNomeInput.val('');
+		this.skuOuNomeInput.focus();
 	}
 	
 	function template(nome, cerveja) {
